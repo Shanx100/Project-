aa5139aaac282f5b4d8f044180b30c1151898c31
Shayne Max <mayisashayne9@gmail.com>
2025-07-15 20:02:49 +0530

    Anime Searchbar

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-let avilableKeyords = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "How to learn programming",
-    "How to center a div",
-    "React",
-    "TypeScript",
-    "How to create a website",
-    "Frontent Development Courses",
-    "Backend Development Courses",
+let avilableKeywords = [
+    "Naruto",
+    "One Piece",
+    "Attack on Titan",
+    "Death Note",
+    "Fullmetal Alchemist: Brotherhood",
+    "Dragon Ball Z",
+    "My Hero Academia",
+    "Demon Slayer",
+    "Jujutsu Kaisen",
+    "Hunter x Hunter",
+    "Bleach",
+    "Sword Art Online",
+    "Tokyo Ghoul",
+    "One Punch Man",
+    "Black Clover",
+    "Haikyuu!!",
+    "Fairy Tail",
+    "Code Geass",
+    "Steins;Gate",
+    "Mob Psycho 100",
+    "JoJo's Bizarre Adventure",
+    "Re:Zero",
+    "The Promised Neverland",
+    "Dr. Stone",
+    "Blue Exorcist",
+    "Noragami",
+    "Cowboy Bebop",
+    "Your Lie in April",
+    "Violet Evergarden",
+    "Danganronpa"
 ]
-const resultbox = document.querySelector('.result-box');
-const Inputbox = document.getElementById("Input-box");
+
+const Inputbox = document.getElementById("Inputbox");
+const resultbox = document.querySelector('.resultbox');
 
 Inputbox.onkeyup = function () {
+    input = Inputbox.value;
     let result = [];
-    let input = Inputbox.value;
 
-    if (input.length) {
-        result = avilableKeyords.filter((keyword) => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        })
-    }
+    result = avilableKeywords.filter((keyword) => {
+        return keyword.toLowerCase().includes(input.toLowerCase());
+    })
     display(result);
-    if (!result.length) {
-        resultbox.innerHTML = "";
-    }
+
+
 }
 function display(result) {
     const content = result.map((list) => {
-        return "<li onclick=selectinput(this)>" + list + "</li>"
+        return "<li onclick=selectitem(this)>" + list + "</li>"
     })
     resultbox.innerHTML = "<ul>" + content.join('') + "</ul>";
+
 }
-function selectinput(list) {
+function selectitem(list) {
     Inputbox.value = list.innerHTML;
     resultbox.innerHTML = "";
 }
