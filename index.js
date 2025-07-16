860d4650e66566b3430307ef69ce8fe2e85cf913
Shayne Max <mayisashayne9@gmail.com>
2025-07-16 19:08:18 +0530

    Heartbeat Animation

--- index.js
+++ index.js
@@ -1,58 +1,8 @@
-let avilableKeywords = [
-    "Naruto",
-    "One Piece",
-    "Attack on Titan",
-    "Death Note",
-    "Fullmetal Alchemist: Brotherhood",
-    "Dragon Ball Z",
-    "My Hero Academia",
-    "Demon Slayer",
-    "Jujutsu Kaisen",
-    "Hunter x Hunter",
-    "Bleach",
-    "Sword Art Online",
-    "Tokyo Ghoul",
-    "One Punch Man",
-    "Black Clover",
-    "Haikyuu!!",
-    "Fairy Tail",
-    "Code Geass",
-    "Steins;Gate",
-    "Mob Psycho 100",
-    "JoJo's Bizarre Adventure",
-    "Re:Zero",
-    "The Promised Neverland",
-    "Dr. Stone",
-    "Blue Exorcist",
-    "Noragami",
-    "Cowboy Bebop",
-    "Your Lie in April",
-    "Violet Evergarden",
-    "Danganronpa"
-]
-
-const Inputbox = document.getElementById("Inputbox");
-const resultbox = document.querySelector('.resultbox');
-
-Inputbox.onkeyup = function () {
-    input = Inputbox.value;
-    let result = [];
-
-    result = avilableKeywords.filter((keyword) => {
-        return keyword.toLowerCase().includes(input.toLowerCase());
-    })
-    display(result);
+const heart = document.querySelector(".heart");
+const audio = document.getElementById("Audio");
 
 
-}
-function display(result) {
-    const content = result.map((list) => {
-        return "<li onclick=selectitem(this)>" + list + "</li>"
-    })
-    resultbox.innerHTML = "<ul>" + content.join('') + "</ul>";
-
-}
-function selectitem(list) {
-    Inputbox.value = list.innerHTML;
-    resultbox.innerHTML = "";
-}
+heart.addEventListener("animationiteration", () => {
+    audio.currentTime = 0;
+    audio.onplay();
+})
