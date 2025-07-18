8d1cb1a4269d1fd4f9d85f72a66077cc110d8208
Shayne Max <mayisashayne9@gmail.com>
2025-07-18 21:04:38 +0530

    Random Word Generator

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-const Clock = document.getElementById("Clock");
+const list = document.querySelector(".list");
+const Button = document.getElementById("Button");
+const sound = document.getElementById("sound");
+const select = document.getElementById("select");
 
+let AllChars = [
+    "Apple",     // A
+    "Banana",    // B
+    "Cat",       // C
+    "Dog",       // D
+    "Elephant",  // E
+    "Fish",      // F
+    "Giraffe",   // G
+    "House",     // H
+    "Ice",       // I
+    "Jelly",     // J
+    "Kite",      // K
+    "Lion",      // L
+    "Monkey",    // M
+    "Nest",      // N
+    "Orange",    // O
+    "Penguin",   // P
+    "Queen",     // Q
+    "Rabbit",    // R
+    "Sun",       // S
+    "Tiger",     // T
+    "Umbrella",  // U
+    "Violin",    // V
+    "Whale",     // W
+    "Xylophone", // X
+    "Yak",       // Y
+    "Zebra"      // Z
+];
 
-function updateClock() {
-    const currrenttime = new Date();
-    const hours = currrenttime.getHours().toString().padStart(2, 0);
-    const minutes = currrenttime.getMinutes().toString().padStart(2, 0);
-    const seconds = currrenttime.getSeconds().toString().padStart(2, 0);
-    const timestring = `${hours}:${minutes}:${seconds}`
-    Clock.textContent = timestring;
+Button.addEventListener("click", () => {
+    select.currentTime = 0;
+    select.play();
+    Button.style.display = "none";
+    setTimeout(() => {
+        sound.loop = true;
+        sound.currentTime = 0;
+        sound.play();
+    }, 5000);
+
+    list.style.display = "block";
+
+})
+
+function displaywords() {
+    let word = "";
+    for (let i = 0; i < 1; i++) {
+        const randomindex = Math.floor(Math.random() * AllChars.length);
+        word += AllChars[randomindex];
+    }
+    list.textContent = word;
 }
-updateClock();
-setInterval(updateClock, 1000);
+displaywords();
+setInterval(displaywords, 5000);
