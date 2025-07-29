fe30a89d0aef63f708ff7a1ff30ffa26254c376d
Shayne Max <mayisashayne9@gmail.com>
2025-07-29 18:32:33 +0530

    Searchbar

--- index.js
+++ index.js
@@ -1,62 +1,60 @@
+const avilablekeywords = [
+  "The Dark Knight",
+  "Avengers: Endgame",
+  "Spider-Man: No Way Home",
+  "Iron Man",
+  "Black Panther",
+  "Captain America: The Winter Soldier",
+  "Wonder Woman",
+  "Thor: Ragnarok",
+  "Doctor Strange",
+  "Guardians of the Galaxy",
+  "Man of Steel",
+  "Deadpool",
+  "The Avengers",
+  "Shazam!",
+  "Ant-Man",
+  "The Incredible Hulk",
+  "Justice League",
+  "X-Men: Days of Future Past",
+  "Batman Begins",
+  "Eternals"
+];
+const resultbox = document.getElementById("Display");
+const search = document.getElementById("Search");
+const Button = document.getElementById("Button");
 
 
+search.onkeyup = function () {
+  let results = [];
+  let input = search.value;
 
-Button.onclick = function () {
-    const personname = document.getElementById("Name").value;
-    const Deposit = document.getElementById("Deposit").value;
-    const withdrawal = document.getElementById("Withdrawal").value;
-    const Button = document.getElementById("Button")
-    const Result = document.querySelector(".Result")
-    const sound = document.getElementById("sound");
-    const processing = document.getElementById("Processing");
-
-
-    if (Deposit === "" & withdrawal === "") {
-        window.alert("Please enter specific amounts")
-        return;
+  if (input.length) {
+    results = avilablekeywords.filter((keyword) => {
+      if (keyword.toLowerCase().includes(input.toLowerCase())) {
+        return keyword;
+      }
     }
 
+    )
+  }
+  display(results);
 
 
-    class BankAcc {
-        constructor(owner) {
-            this.owner = owner;
-            this.balance = 0;
-        }
-        deposit(deposit) {
-            this.balance += deposit;
-            return `Congratulations on depositing $${deposit} ${this.owner}`
-        }
-        withdrawal(Withdrawal) {
-            this.balance -= Withdrawal;
-            if (this.balance < Withdrawal) {
-                return "You have insufficient balance to make this withdrawal"
-            }
-            else {
-                return `Congratulations ${this.owner} on successfully withdrawing ${Withdrawal}`;
-            }
-        }
-        displaybalance() {
-            return `Your current Balance is $${this.balance}`
-        }
-    }
-    const person = new BankAcc(personname);
-    let info = [];
-    info.push(person.deposit(Deposit));
-    info.push(person.withdrawal(withdrawal));
-    info.push(person.displaybalance());
-    let content = [];
-    content = info.map((item) => {
-        return `<li>` + item + `</li>`
-    })
-    sound.currentTime = 0;
-    sound.play();
-    processing.textContent = `Processing Transaction!!!.Please wait for a few seconds`
-
-    setTimeout(function () {
-        processing.textContent = "";
-        Result.innerHTML = `<ul>` + content.join("") + `</ul>`;
-    }, 10000);
 
 }
+function display(results) {
+  const content = results.map((list) => {
+    return `<li onclick=selectinput(this)>` + list + `</li>`
+  })
+  resultbox.innerHTML = `<ul>` + content.join("") + `</ul>`;
+}
 
+function selectinput(list) {
+  search.value = list.innerHTML;
+  resultbox.innerHTML = "";
+}
+
+Button.onclick = function () {
+  search.value = "";
+}
