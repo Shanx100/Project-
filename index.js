684ab2d7428faa64d3d5b423d9c546786f2fbbc9
Shayne Max <mayisashayne9@gmail.com>
2025-07-31 15:30:33 +0530

    Anime List

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const hidesidebar = document.getElementById("hidesidebar");
-const showsidebar = document.getElementById("showsidebar");
 
-hidesidebar.onclick = function () {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = "none";
-}
-showsidebar.onclick = function () {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = "flex";
-}
+
+
+
